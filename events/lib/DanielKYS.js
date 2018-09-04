f3e017cd3a0d9a92be2fe29cbb1e7d76e9c006ba
Justin Mulligan <jtmmulligan@gmail.com>
2018-09-04 00:43:50 -0700

    added todo in daniel kys

--- events/lib/DanielKYS.js
+++ events/lib/DanielKYS.js
@@ -1,6 +1,6 @@
 module.exports = async (bot, message) => {
 	console.log("DanielKYS listener activated...");
-
+    //todo: global discord emojis dont work like this, so make it work
     message.react(bot.emojis.find(val => val.name == "regional_indicator_k"));
     message.react(bot.emojis.find(val => val.name == "regional_indicator_y"));
     message.react(bot.emojis.find(val => val.name == "regional_indicator_s"));
