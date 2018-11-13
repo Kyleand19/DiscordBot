7d35c58ca4868eca477da9dac20f2cd6e3e62ba4
Kyle Stadelmann <dlice@hotmail.com>
2018-11-12 17:48:42 -0800

    Finished listRoles cmd, started role command

--- Commands/list_roles.js
+++ Commands/list_roles.js
@@ -3,7 +3,7 @@ const discord = require('discord.js');
 
 module.exports.help = {
     commandName: "listroles",
-	description: "Lists the roles on this server."
+	description: "Lists the 'main' and 'player' roles on the BD4 server."
 }
 
 module.exports.run = async (bot, msg, args) => {
@@ -16,7 +16,7 @@ module.exports.run = async (bot, msg, args) => {
 
 	// sort the main roles
 	mainRoles = mainRoles.sort((a,b) => {
-		b.comparePositionTo(a);
+		return b.comparePositionTo(a);
 	});
 
 	// grab the player roles
@@ -31,9 +31,23 @@ module.exports.run = async (bot, msg, args) => {
 		if (a.name > b.name) return 1;
 	});
 
+	let mainRoleStr = "";
+	mainRoles.forEach((value, key, collection) => {
+		mainRoleStr += value.name;
+		mainRoleStr += "\n";
+	});
+
+	let playerRoleStr = "";
+	playerRoles.forEach((value, key, collection) => {
+		playerRoleStr += value.name;
+		playerRoleStr += "\n";
+	});
+
 	var roleInfo = new discord.RichEmbed()
-		.addField("Main Roles", "test")
+		.addField("Main Roles", mainRoleStr)
+		.addField("Player Roles", playerRoleStr)
 		.setThumbnail(bot.guilds.get(bot.constants.BD4_ID).iconURL)
+		.setColor(0x0)
 
 	msg.channel.send(roleInfo);
 
--- Commands/players_role.js
+++ Commands/players_role.js
@@ -1,9 +1,9 @@
 module.exports.help = {
 	commandName: "role",
-	description: "Assigns players roles. Use the listRoles command to display the avaliable player roles."
+	description: "Assigns 1 'players role.' Use the listRoles command to display the avaliable player roles."
 }
 
 module.exports.run = async (bot, msg, args) => {
-	let player = msg.mentions.roles.first;
+	if (args)
 	console.log(bot.roles.players.has(player.id));
 }
