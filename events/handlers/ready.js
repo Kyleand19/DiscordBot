ca86f26ab4ff82e7dce496419de867dcf9630a61
Justin Mulligan <jtmmulligan@gmail.com>
2020-07-14 01:25:12 -0700

    added dirty implementation of status rotation

--- events/handlers/ready.js
+++ events/handlers/ready.js
@@ -3,5 +3,54 @@ module.exports = async (bot) => {
     console.log("Bot starting up...");
     bot.printSpace();
 
-    bot.user.setActivity("With Khang's Catgirls");
+    // Status rotation
+    var i = 1;
+    var statuses = 11;
+    function statusLoop() {
+        setTimeout(function () {
+            switch (i % statuses) {
+                case 1:
+                    bot.user.setActivity("With Khang's Catgirls");
+                    break;
+                case 2:
+                    bot.user.setActivity("Kyle is there a reason why I was demodded?");
+                    break;
+                case 3:
+                    bot.user.setActivity("Guten");
+                    break;
+                case 4:
+                    bot.user.setActivity("Valhen");
+                    break;
+                case 5:
+                    bot.user.setActivity("Skowhen");
+                    break;
+                case 6:
+                    bot.user.setActivity("HanabiHen");
+                    break;
+                case 7:
+                    bot.user.setActivity("Toontownhen");
+                    break;
+                case 8:
+                    bot.user.setActivity("Increasing Daniel's WPM...");
+                    break;
+                case 9:
+                    bot.user.setActivity("Watching Kyle blow more money on maple");
+                    break;
+                case 10:
+                    bot.user.setActivity("With your permissions");
+                    i = 1;
+                    break;
+                default:
+                    bot.user.setActivity("THIS IS AN ERROR LOL");
+                    i = 1;
+            }
+            i++;
+            if (i < statuses){
+                statusLoop();
+            }
+            // 10 minute intervals
+        }, 600000)
+    }
+
+    statusLoop();
 };
