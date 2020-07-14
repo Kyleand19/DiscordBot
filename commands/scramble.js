b6a5ede1052854207464fc91a3025608b822e742
Kyle Stadelmann <dlice@hotmail.com>
2020-07-13 23:30:51 -0700

    Fixed bugs with scramble, working correctly

--- commands/scramble.js
+++ commands/scramble.js
@@ -1,3 +1,5 @@
+const discord = require('discord.js');
+
 module.exports.help = {
 	commandName: "scramble",
 	description: "Sends everyone in your channel to a random channel.",
@@ -15,20 +17,30 @@ module.exports.run = async (bot, msg, args) => {
 		return false;
 	}
 
+	let validChannels = new discord.Collection();
+
+	// Gets all valid channels 
+	// (that are voice, another channel thatn current, and that everyone can see)
+	msg.guild.channels.cache.forEach(channel => {
+		let perm = channel.permissionsFor(msg.guild.roles.everyone).has("VIEW_CHANNEL");
+		if (channel.type === "voice" && channel.id !== voiceChannel.id && perm) {
+			validChannels.set(channel.id, channel);
+		}
+	});
+
+	// Test if there exists valid channels
+	if (validChannels.array().length <= 0) {
+		msg.channel.send("Scramble failed because there are not valid voice channels");
+		return false;
+	}
+
 	msg.channel.send(`Initiating channel scramble on ${voiceChannel.name}.`);
 
 	// Collection of people in the message member's channel
 	let channelMembers = voiceChannel.members;
 
 	channelMembers.forEach(victim => {
-		let randomChannel;
-		do {
-			randomChannel = msg.guild.channels.cache.random();
-			// Keep generating random channels if its not a voicechannel, or the channel they're 
-			// already in, or if not everyone has permission to view that channel
-		} while (randomChannel.type !== "voice" || randomChannel.id === voiceChannel.id 
-			|| randomChannel.permissionsFor(msg.guild.roles.everyone).VIEW_CHANNEL);
-
+		let randomChannel = validChannels.random();
 		victim.edit({channel: randomChannel});
 	});
 
