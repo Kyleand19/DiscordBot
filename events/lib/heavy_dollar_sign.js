06be3a34bbeedbd30da2d8efd0807bf9d9a34048
Justin Mulligan <jtmmulligan@gmail.com>
2020-07-13 20:46:57 -0700

    dumb mistake in heavy_dollar_sign.js

--- events/lib/heavy_dollar_sign.js
+++ events/lib/heavy_dollar_sign.js
@@ -2,6 +2,6 @@
 module.exports = async (bot, message) => {
 
 	if (bot.util.random(require("../event_percentages.js").HEAVY_DOLLAR_SIGN)) {
-    message.react(constants.HEAVY_DOLLAR_SIGN);
+    message.react(bot.constants.HEAVY_DOLLAR_SIGN);
 	} 
 }
