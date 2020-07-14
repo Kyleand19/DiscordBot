0d709fba4d3e3db22b3cce009438ab3dbc92387a
Justin Mulligan <jtmmulligan@gmail.com>
2020-07-14 02:59:50 -0700

    made statLoop() more readable

--- events/handlers/ready.js
+++ events/handlers/ready.js
@@ -13,10 +13,7 @@ module.exports = async (bot) => {
         setTimeout(function () {
             i = Math.floor(Math.random() * possibleStatuses);
             bot.user.setActivity(statusMessages[i]);
-            statLoop();
             // New status every 5 minutes
         }, 300000)
     }
-
-    statLoop();
 };
