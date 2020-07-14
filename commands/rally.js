f39e1d5587327783c3152fb9b619a95a9f458730
Zachary Tulman <ztulman@ucsd.edu>
2020-07-14 15:55:58 -0700

    Added role ping capability to rally

--- commands/rally.js
+++ commands/rally.js
@@ -3,7 +3,7 @@ const discord = require('discord.js');
 module.exports.help = {
     commandName: "rally",
 	description: "Brings all users connected to voice to the voice channel that the user is in.",
-	usage: `rally`,
+    usage: `rally`,
 }
 
 module.exports.disabled = false;
@@ -14,6 +14,7 @@ module.exports.run = async (bot, message, args) => {
     let memberCounter = 0;
     let pullToHidden = false;
     let publicChannel = voiceChannel.permissionsFor(message.guild.roles.everyone).has("VIEW_CHANNEL");
+    let roleCall = message.mentions.roles.first();
 
     if (args[0] === "to" && args[1] === "me") {
         pullToHidden = true;
@@ -44,7 +45,9 @@ module.exports.run = async (bot, message, args) => {
 
     validChannels.forEach(vChannel => {
         vChannel.members.forEach(validMember => {
-            memberCounter++;
+            if (!(roleCall && !validMember.roles.cache.get(roleCall.id))) {
+                memberCounter++;
+            }
         });
     });
     if (memberCounter <= 0) {
@@ -58,13 +61,15 @@ module.exports.run = async (bot, message, args) => {
 
     // Move all valid users to caller's voice channel
     validChannels.forEach(userChannel => {
-    userChannel.members.forEach(member => {
-        console.log("Moving user with ID: " + member.id);
-        member.edit({channel: message.member.voice.channel})
-            .catch((error) => {
-                console.error(error);
+        userChannel.members.forEach(member => {
+            if (!(roleCall && !member.roles.cache.get(roleCall.id))) {
+                console.log("Moving user with ID: " + member.id);
+                member.edit({channel: message.member.voice.channel})
+                    .catch((error) => {
+                        console.error(error);
+                    });
+                }
             });
-        });
     });
 
     message.channel.send("Rally completed!");
