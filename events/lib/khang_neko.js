6726c5965494329aae683ce5df78e5e861183a5a
Kyle Stadelmann <dlice@hotmail.com>
2020-07-18 16:34:09 -0700

    Fixed khang neko emoji spawning event

--- events/lib/khang_neko.js
+++ events/lib/khang_neko.js
@@ -2,6 +2,6 @@
 module.exports = async (bot, message) => {
 
     if (bot.util.random(require("../event_percentages.js").KHANG_NEKO_CHANCE)) {
-        message.react(bot.emojis.find(val => val.name == "cat_thonk"));
+        message.react(bot.emojis.cache.find(val => val.name === bot.constants.KHANG_NEKO_EMOJI));
     }
 }
