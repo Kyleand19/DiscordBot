af4d0ba5b51026673d8964e376841211a335c9f4
Zachary Tulman <ztulman@ucsd.edu>
2021-04-02 04:51:28 -0700

    Added logging when user gets muted

--- events/lib/guildMemberSpeaking/asian_kyle_random_mute.js
+++ events/lib/guildMemberSpeaking/asian_kyle_random_mute.js
@@ -2,6 +2,7 @@
 module.exports = async (bot, member, speakingState) => {
     // member.id !== bot.constants.ASIAN_KYLE_ID <-- replace the true with this
     if (true && bot.util.random(bot.event_percentages.MUTE_CHANCE)) {
+        console.log("Server muting member: " + member.id);
         member.edit({ mute: true });
     }
 }
